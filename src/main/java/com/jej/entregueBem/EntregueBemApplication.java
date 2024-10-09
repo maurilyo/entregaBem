ff8c7a4649ee5fcb39be90cde4b7c769eb5dc065
maurilyo <maurilyo@gmail.com>
2024-10-09 15:33:06 -0300

    Adicionando confg Feign

--- src/main/java/com/jej/entregueBem/EntregueBemApplication.java
+++ src/main/java/com/jej/entregueBem/EntregueBemApplication.java
@@ -2,7 +2,9 @@ package com.jej.entregueBem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class EntregueBemApplication {
 
