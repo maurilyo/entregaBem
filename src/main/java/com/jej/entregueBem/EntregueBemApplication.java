e66bdc77c8cc7b477a876ff37fafcb042b0cd646
maurilyo <maurilyo@gmail.com>
2024-10-09 15:32:15 -0300

    Commit inicial

---
+++ src/main/java/com/jej/entregueBem/EntregueBemApplication.java
@@ -0,0 +1,13 @@
+package com.jej.entregueBem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregueBemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregueBemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jej/entregueBem/EntregueBemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jej.entregueBem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregueBemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
